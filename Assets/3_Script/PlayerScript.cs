88eec2d5d4fd58c9cfabac77ead00c5053aa3684
iyrivin <irinative@gmail.com>
2015-08-05 00:57:16 +0900

    DeleteCollider
    DeleteCollider Added

--- Assets/3_Script/PlayerScript.cs
+++ Assets/3_Script/PlayerScript.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	public float _speed = 5.0f;
+	public UISprite _FeverBarWidget;
 	//public void Awake(){
 	//	Animation ["1_damage"].speed = 2;
 	//}
 	// Use this for initialization
 	void Start () {
-	
+		_FeverBarWidget.fillAmount = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		_FeverBarWidget.fillAmount = 0.5f;
 	}
 
 	public int _hp=100;
